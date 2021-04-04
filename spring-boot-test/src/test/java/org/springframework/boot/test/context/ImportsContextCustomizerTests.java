71b4fef0c373d897373946b8022d36d2c10730bc
z-dk <37075762+z-dk@users.noreply.github.com>
2021-04-04 12:04:45 +0800

    Initialize 1.4.x

---
+++ spring-boot-test/src/test/java/org/springframework/boot/test/context/ImportsContextCustomizerTests.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2012-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.test.context;
+
+import kotlin.Metadata;
+import org.junit.Test;
+import org.spockframework.runtime.model.SpecMetadata;
+import spock.lang.Issue;
+import spock.lang.Stepwise;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link ImportsContextCustomizer}.
+ *
+ * @author Andy Wilkinson
+ */
+public class ImportsContextCustomizerTests {
+
+	@Test
+	public void customizersForTestClassesWithDifferentKotlinMetadataAreEqual() {
+		assertThat(new ImportsContextCustomizer(FirstKotlinAnnotatedTestClass.class))
+				.isEqualTo(new ImportsContextCustomizer(
+						SecondKotlinAnnotatedTestClass.class));
+	}
+
+	@Test
+	public void customizersForTestClassesWithDifferentSpockFrameworkAnnotationsAreEqual() {
+		assertThat(
+				new ImportsContextCustomizer(FirstSpockFrameworkAnnotatedTestClass.class))
+						.isEqualTo(new ImportsContextCustomizer(
+								SecondSpockFrameworkAnnotatedTestClass.class));
+	}
+
+	@Test
+	public void customizersForTestClassesWithDifferentSpockLangAnnotationsAreEqual() {
+		assertThat(new ImportsContextCustomizer(FirstSpockLangAnnotatedTestClass.class))
+				.isEqualTo(new ImportsContextCustomizer(
+						SecondSpockLangAnnotatedTestClass.class));
+	}
+
+	@Metadata(d2 = "foo")
+	static class FirstKotlinAnnotatedTestClass {
+
+	}
+
+	@Metadata(d2 = "bar")
+	static class SecondKotlinAnnotatedTestClass {
+
+	}
+
+	@SpecMetadata(filename = "foo", line = 10)
+	static class FirstSpockFrameworkAnnotatedTestClass {
+
+	}
+
+	@SpecMetadata(filename = "bar", line = 10)
+	static class SecondSpockFrameworkAnnotatedTestClass {
+
+	}
+
+	@Stepwise
+	static class FirstSpockLangAnnotatedTestClass {
+
+	}
+
+	@Issue("1234")
+	static class SecondSpockLangAnnotatedTestClass {
+
+	}
+
+}
---
+++ spring-boot-tools/spring-boot-gradle-plugin/src/main/java/org/springframework/boot/gradle/plugin/SpringBootPlugin.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2012-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.gradle.plugin;
+
+import org.gradle.api.Action;
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.api.plugins.JavaPlugin;
+import org.gradle.api.tasks.compile.JavaCompile;
+import org.gradle.util.GradleVersion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.gradle.SpringBootPluginExtension;
+import org.springframework.boot.gradle.agent.AgentPluginFeatures;
+import org.springframework.boot.gradle.dependencymanagement.DependencyManagementPluginFeatures;
+import org.springframework.boot.gradle.repackage.RepackagePluginFeatures;
+import org.springframework.boot.gradle.run.RunPluginFeatures;
+
+/**
+ * Gradle 'Spring Boot' {@link Plugin}.
+ *
+ * @author Phillip Webb
+ * @author Dave Syer
+ * @author Andy Wilkinson
+ */
+public class SpringBootPlugin implements Plugin<Project> {
+
+	private static final Logger logger = LoggerFactory.getLogger(SpringBootPlugin.class);
+
+	@Override
+	public void apply(Project project) {
+		if (GradleVersion.current().compareTo(GradleVersion.version("2.9")) < 0) {
+			logger.warn("Spring Boot plugin's support for Gradle "
+					+ GradleVersion.current().getVersion()
+					+ " is deprecated. Please upgrade to Gradle 2.9 or later.");
+		}
+		project.getExtensions().create("springBoot", SpringBootPluginExtension.class,
+				project);
+		project.getPlugins().apply(JavaPlugin.class);
+		new AgentPluginFeatures().apply(project);
+		new RepackagePluginFeatures().apply(project);
+		new RunPluginFeatures().apply(project);
+		new DependencyManagementPluginFeatures().apply(project);
+		project.getTasks().withType(JavaCompile.class).all(new SetUtf8EncodingAction());
+	}
+
+	private static class SetUtf8EncodingAction implements Action<JavaCompile> {
+
+		@Override
+		public void execute(final JavaCompile compile) {
+			compile.doFirst(new Action<Task>() {
+
+				@Override
+				@SuppressWarnings("deprecation")
+				public void execute(Task t) {
+					if (compile.getOptions().getEncoding() == null) {
+						compile.getOptions().setEncoding("UTF-8");
+					}
+				}
+
+			});
+		}
+
+	}
+
+}
